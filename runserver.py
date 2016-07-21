51bd137d29c67c3d3110902ffa232c9592b84e8b
Valentyn Yakovlev <valentyn.yakovlev@gmail.com>
2016-07-21 11:10:32 +0300

    add file

---
+++ runserver.py
@@ -0,0 +1,45 @@
+from socket import *
+
+host = 'localhost'
+port = 8888
+addr = (host, port)
+
+tcp_socket = socket(AF_INET, SOCK_STREAM)
+tcp_socket.bind(addr)
+tcp_socket.listen(1)
+
+while True:
+    print('wait connection...')
+
+    conn, addr = tcp_socket.accept()
+    print('client addr: ', addr)
+    data = conn.recv(1024)
+
+    if not data:
+        #conn.send(b'ERROR!\n')
+        conn.close()
+
+    elif data == b'date\n':
+        conn.send(b'DATE!\n')
+        conn.close()
+
+    elif data == b'help\n':
+        conn.send(b'''Available commands:
+            date: Return current date and time
+            myip: Return client's IP address
+            help: See this help message
+            ''')
+        conn.close()
+
+    elif data == b'myip\n':
+        conn.send(str.encode(addr[0]) + b'\n')
+        conn.close()
+    else:
+        print(data)
+        enc = data.decode('utf-8')
+        enc = enc.rstrip('\n')
+        conn.send(b'ERROR! The command ' + str.encode(enc) + b' is unknown! Use "help" to see all available commands.\n')
+        conn.close()
+
+tcp_socket.close()
+
